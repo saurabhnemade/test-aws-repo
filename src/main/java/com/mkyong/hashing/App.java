321fa0dbfa70c60a315705eeb250b9641311874e
Saurabh Nemade <saurabhnemade@users.noreply.github.com>
2019-04-26 17:25:07 +0530

    updated command

--- src/main/java/com/mkyong/hashing/App.java
+++ src/main/java/com/mkyong/hashing/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
-        String command = "java -jar jenkins-cli.jar -s " + args[0] + " -auth admin:" + args[1] + " create-credentials-by-xml system::system::jenkins < /tmp/cred.xml";
+        String command = "cat /tmp/cred.xml | java -jar ./jenkins-cli.jar -s " + args[0] + " -auth admin:" + args[1] + " create-credentials-by-xml system::system::jenkins _";
         System.out.println("Executing: " + command);
         ProcessBuilder builder = new ProcessBuilder(command);
         Process process = builder.start();
